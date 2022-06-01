60b6597279561ceb2acbb93cf10373df80078c90
Patrick John Fernandez <106681178+WarSnoopy@users.noreply.github.com>
2022-06-01 22:09:25 +0800

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+var password;
+var pass1 = "SuperPat";
+
+password = prompt("Please enter your password to view this page!");
+
+if (password==pass1) {
+	alert("Password is Correct! Click OK to enter");
+}
+
+else {
+	window.location = "practice2.html";
+}
